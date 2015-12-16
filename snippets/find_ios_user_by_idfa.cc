ed5b1789835044954e3817de302c737afce9042a
Alex Zolotarev <alex@maps.me>
2015-12-16 16:03:30 +0300

    Find iOS user's AlohaID by IDFA.

---
+++ snippets/find_ios_user_by_idfa.cc
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ The MIT License (MIT)
+
+ Copyright (c) 2015 Alexander Zolotarev <me@alex.bio> from Minsk, Belarus
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ *******************************************************************************/
+
+#include "../include/processor_light.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+
+  if (argc < 2) {
+    cout << "Prints AlohaID of user with given IDFA." << endl;
+    cout << "Usage: " << argv[0] << " IDFA" << endl;
+    return -1;
+  }
+
+  string const IDFA = argv[1];
+
+  alohalytics::ProcessorLight([&IDFA](const AlohalyticsIdServerEvent * se, const AlohalyticsKeyEvent * e) {
+    if (e->key == "$iosDeviceIds") {
+      const auto & pairs = static_cast<const AlohalyticsKeyPairsEvent *>(e)->pairs;
+      const auto found = pairs.find("advertisingIdentifier");
+      if (found != pairs.end() && IDFA == found->second) {
+        cout << se->id << " user has IDFA=" << IDFA << endl;
+      }
+    }
+  });
+  return 0;
+}
