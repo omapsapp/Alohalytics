7619316e72ff847e2eb589118f6343534814ccdb
burivuh <burivuh@maps.me>
2016-07-19 13:42:58 +0300

    improved logging of event creation

--- snippets/pyaloha/event_factory.py
+++ snippets/pyaloha/event_factory.py
@@ -1,4 +1,5 @@
 import itertools
+import traceback
 
 from pyaloha.event import Event
 
@@ -7,14 +8,21 @@ class EventFactory(object):
     def __init__(self, custom_events):
         self.check_events(custom_events)
 
-        self.registered = dict(
-            (key, event_cls)
+        self.registered = {
+            key: event_cls
             for event_cls in custom_events
             for key in event_cls.keys
-        )
+        }
 
     def make_event(self, key, *args, **kwargs):
-        return self.registered.get(key, Event)(key, *args, **kwargs)
+        try:
+            return self.registered.get(key, Event)(key, *args, **kwargs)
+        except Exception as exc:
+            raise Exception(
+                'Event "%s" creation failed: %s' % (
+                    key, traceback.format_exc(exc)
+                )
+            )
 
     @classmethod
     def check_events(cls, custom_events):
@@ -22,4 +30,4 @@ class EventFactory(object):
             event_cls.keys for event_cls in custom_events
         ))
         if len(frozenset(event_keys)) != len(event_keys):
-            raise ImportError('Intersection of keys in events')
+            raise ImportError('Keys intersection in events')
