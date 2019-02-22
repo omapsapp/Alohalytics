ea825ee5a851d815d67b3bb6aa58675cb168b601
burivuh <burivuh@maps.me>
2019-02-22 15:37:00 +0300

    modified search event

--- snippets/pysnip/events/misc.py
+++ snippets/pysnip/events/misc.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
+
 from pyaloha.event import Event, DictEvent
-from pyaloha.ccode import c_string, c_unicode
 
 
 # ALOHA: searchEmitResults [
@@ -12,6 +13,16 @@ from pyaloha.ccode import c_string, c_unicode
 #   viewportMaxX=-71.9639 viewportMaxY=-13.6282
 #   viewportMinX=-71.998 viewportMinY=-13.663
 # ]
+#
+# ALOHA (after Apr 2016): searchEmitResultsAndCoords [
+#   locale=es_US
+#   posX=-57.9893 posY=-33.1022
+#   query=amor results=10
+#   Doctor José G. Amorín|Calle|1   La Amarilla|place-neighbourhood|0
+#   viewportMaxX=-57.9845 viewportMaxY=-33.101
+#   viewportMinX=-57.9951 viewportMinY=-33.1085
+# ]
+
 
 class SearchResults(DictEvent):
     keys = (
@@ -22,22 +33,59 @@ class SearchResults(DictEvent):
         super(SearchResults, self).__init__(*args, **kwargs)
 
         try:
-            self.query = self.data['query']
-        except KeyError as old_type_event:
-            self.query, self.result_num = self.data.items()[0]
+            raw_query = self.data['query']
+        except KeyError as is_old_type_event:
+            # TODO: investigate bug in c_api
+            for raw_query, self.result_num in self.data.items():
+                if raw_query:
+                    break
         else:
             results = filter(None, self.data['results'].split())
             self.result_num = int(results[0])
             self.results = results[1:]
 
+        self.invalid_query = True  # empty-like queries are set as invalid
+        query = raw_query.decode('utf8').encode('utf8')
+        if query:
+            self.query = query.strip()
+
+            if self.query:
+                self.invalid_query = False
+
+        try:
+            self.gps = map(
+                float, (
+                    self.data['posX'], self.data['posY']
+                )
+            )
+        except (ValueError, KeyError):  # bad float, old type event
+            self.gps = None
+
+        try:
+            self.viewport = {
+                'maxx': float(self.data['viewportMaxX']),
+                'maxy': float(self.data['viewportMaxY']),
+                'minx': float(self.data['viewportMinX']),
+                'miny': float(self.data['viewportMinY'])
+            }
+        except (ValueError, KeyError):  # bad float, old type event:
+            self.viewport = None
+
+        self.locale = self.data.get('locale')
+
+        del self.data
+
     def process_me(self, processor):
         processor.process_search_results(self)
 
     def __dumpdict__(self):
-        d = super(DictEvent, self).__dumpdict__()
-
-        d['query'] = self.query
-        d['result_num'] = self.result_num
+        d = super(SearchResults, self).__basic_dumpdict__()
+        d.update({
+            'query': self.query,
+            'gps': self.gps,
+            'viewport': self.viewport,
+            'locale': self.locale
+        })
         return d
 
 
