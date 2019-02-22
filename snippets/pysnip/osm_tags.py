3f2e34eea2cecb0010ac204dca79a946fcb4c352
burivuh <burivuh@maps.me>
2019-02-22 15:39:09 +0300

    added more osm tags to the groups

--- snippets/pysnip/osm_tags.py
+++ snippets/pysnip/osm_tags.py
@@ -1,9 +1,8 @@
-# Groups are sorted by popularity.
+# by popularity
 GROUPS = (
     ('roads', frozenset((
         'highway-pedestrian',
         'highway-pedestrian-area',
-        'highway-service',
         'highway-unclassified',
         'highway-motorway',
         'highway-trunk',
@@ -14,6 +13,12 @@ GROUPS = (
     ))),
     ('hotels', frozenset((
         'sponsored-booking',
+        'tourism-hotel',
+        'tourism-hostel',
+        'tourism-apartment',
+        'tourism-guest_house',
+        'tourism-resort',
+        'tourism-camp_site'
     ))),
     ('food', frozenset((
         'amenity-bar',
@@ -25,25 +30,25 @@ GROUPS = (
     ))),
     ('city_transport', frozenset((
         'railway-station-subway',
+        'railway-subway_entrance',
         'railway-tram_stop',
         'highway-bus_stop'
     ))),
     ('global_transport', frozenset((
         'aeroway-aerodrome',
+        'aeroway-aerodrome-international',
+        'aerialway-station',
         'amenity-ferry_terminal',
         'railway-halt',
         'railway-station',
+        'building-train_station',
         'amenity-bus_station'
     ))),
-    ('all_transport', frozenset((
-        'railway-station-subway',
-        'railway-tram_stop',
-        'highway-bus_stop',
-        'aeroway-aerodrome',
-        'amenity-ferry_terminal',
-        'railway-halt',
-        'railway-station',
-        'amenity-bus_station'
+    ('personal_transport', frozenset((
+        'amenity-parking',
+        'amenity-parking-fee',
+        'amenity-fuel',
+        'highway-service'
     ))),
     ('large_toponyms', frozenset((
         'place-region',
@@ -52,13 +57,15 @@ GROUPS = (
         'place-hamlet',
         'place-continent',
         'place-state',
+        'place-city-capital-6',
         'place-city-capital-4',
         'place-city-capital-2',
         'place-suburb',
         'place-country',
         'place-city',
         'place-town',
-        'place-village'
+        'place-village',
+        'place-island'
     ))),
     ('sightseeing', frozenset((
         'amenity-place_of_worship',
@@ -69,17 +76,35 @@ GROUPS = (
         'waterway-waterfall',
         'natural-cave_entrance',
         'historic-castle',
+        'historic-castle-stately',
+        'historic-castle-defensive',
         'tourism-artwork',
+        'tourism-artwork-statue',
+        'tourism-artwork-sculpture',
         'historic-memorial',
+        'historic-memorial-statue',
         'amenity-place_of_worship-christian',
+        'amenity-place_of_worship-buddhist',
+        'amenity-place_of_worship-hindu',
         'natural-peak',
         'amenity-theatre',
+        'boundary-national_park',
+        'tourism-theme_park',
         'natural-beach',
+        'natural-volcano',
+        'natural-water',
+        'natural-spring',
         'historic-monument',
+        'historic-archaeological_site',
+        'historic-ruins',
         'leisure-park',
         'tourism-attraction',
         'tourism-viewpoint',
-        'tourism-museum'
+        'tourism-museum',
+        'leisure-nature_reserve',
+        'leisure-garden',
+        'place-square',
+        'amenity-arts_centre'
     ))),
     ('shopping', frozenset((
         'shop-clothes',
@@ -87,12 +112,25 @@ GROUPS = (
         'shop',
         'shop-convenience',
         'shop-mall',
-        'shop-supermarket'
+        'shop-supermarket',
+        'shop-department_store',
+        'shop-books'
+    ))),
+    ('greenzone', frozenset((
+        'landuse-forest',
+        'natural-wood',
+        'landuse-farmland',
+        'landuse-meadow'
+    ))),
+    ('health', frozenset((
+        'amenity-pharmacy',
+        'amenity-hospital'
     )))
 )
 
 
-def get_groups_by_tag(tag):
+def get_group_by_tag(tag):
     for group, tag_set in GROUPS:
         if tag in tag_set:
-            yield group
+            return group
+    return None
