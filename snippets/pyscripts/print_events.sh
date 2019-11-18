68a09a907e06bc51a212a451579a15e732a62ffe
alekseik1 <1alekseik1@gmail.com>
2019-11-18 14:27:49 +0300

    fix: reminder for `print_events.sh`

--- snippets/pyscripts/print_events.sh
+++ snippets/pyscripts/print_events.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # First argument - date
 # Second argument - event name
+# Don't forget to place `print_events` binary in the same folder!
 gzip -d -c /home/sites/alohalytics/alohalytics_messages-${1}.gz | ./print_events | grep "${2}"
